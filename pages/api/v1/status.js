6b44a4e0da9833ff69a8ce094a588259596bbb3b
Douglas Fortunato <douglasfortunato@hotmail.com>
2024-04-18 13:54:08 +0000

    Add  e  files

--- pages/api/v1/status.js
+++
@@ -1,5 +0,0 @@
-function status(req, res) {
-  res.status(200).json({ chave: "Valor" });
-}
-
-export default status;
---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,9 @@
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as Sum;");
+  console.log(result.rows);
+  res.status(200).json({ chave: "Valor" });
+}
+
+export default status;
