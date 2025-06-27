72af238c42194a3c2216d881f43948ac9800a392
Douglas Fortunato <douglasfortunato@hotmail.com>
2025-06-27 14:23:39 +0000

    feat: add user migration

---
+++ tests/integration/api/v1/users/post.test.js
@@ -0,0 +1,19 @@
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("POST /api/v1/users", () => {
+  describe("Anonymous user", () => {
+    test("With unique and valid data", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+      });
+
+      expect(response.status).toBe(201);
+    });
+  });
+});
