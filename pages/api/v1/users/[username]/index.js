2fc16c9f0cde166aafe52a759ed8754f9f1a5b95
Douglas Fortunato <douglasfortunato@hotmail.com>
2025-07-10 13:47:26 +0000

    feat: add update user

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -5,12 +5,22 @@ import user from "models/user";
 const router = createRouter();
 
 router.get(getHandler);
+router.patch(patchHandler);
 
 export async function getHandler(req, res) {
   const username = req.query.username;
   const userFound = await user.findOneByUsername(username);
 
   res.status(200).json(userFound);
+}
+
+export async function patchHandler(req, res) {
+  const username = req.query.username;
+  const userInputValues = req.body;
+
+  const updateUser = await user.update(username, userInputValues);
+
+  res.status(200).json(updateUser);
 }
 
 export default router.handler(controller.errorHandlers);
---
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -0,0 +1,130 @@
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("PATCH /api/v1/users/[username]", () => {
+  describe("Anonymous user", () => {
+    test("With nonexistent username", async () => {
+      const response = await fetch(
+        "http://localhost:3000/api/v1/users/usuarioInexistente",
+        {
+          method: "PATCH",
+        },
+      );
+
+      expect(response.status).toBe(404);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "NotFoundError",
+        message: "O username informado não foi encontrado no sistema",
+        action: "Verifique se o username está digitado corretamente",
+        status_code: 404,
+      });
+    });
+
+    test("With duplicated 'username'", async () => {
+      const user1response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "user1",
+          email: "user1@mail.com",
+          password: "senha123",
+        }),
+      });
+
+      const user2response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "user2",
+          email: "user2@mail.com",
+          password: "senha123",
+        }),
+      });
+
+      const response = await fetch("http://localhost:3000/api/v1/users/user2", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "user1",
+        }),
+      });
+
+      const responseBody = await response.json();
+
+      expect(user1response.status).toBe(201);
+      expect(user2response.status).toBe(201);
+      expect(response.status).toBe(400);
+      expect(responseBody).toEqual({
+        name: "ValidationError",
+        message: "O nome de usuário informado já está sendo utilizado.",
+        action: "Utilize outro nome de usuário para realizar esta operação.",
+        status_code: 400,
+      });
+    });
+
+    test("With duplicated 'email'", async () => {
+      const user1response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "email1",
+          email: "email1@mail.com",
+          password: "senha123",
+        }),
+      });
+
+      const user2response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "email2",
+          email: "email2@mail.com",
+          password: "senha123",
+        }),
+      });
+
+      const response = await fetch(
+        "http://localhost:3000/api/v1/users/email2",
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: "email2@mail.com",
+          }),
+        },
+      );
+
+      const responseBody = await response.json();
+
+      expect(user1response.status).toBe(201);
+      expect(user2response.status).toBe(201);
+      expect(response.status).toBe(400);
+      expect(responseBody).toEqual({
+        name: "ValidationError",
+        message: "O email informado já está sendo utilizado.",
+        action: "Utilize outro email para realizar esta operação.",
+        status_code: 400,
+      });
+    });
+  });
+});
